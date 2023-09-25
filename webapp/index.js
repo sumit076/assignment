b09b85c13a03c1ef7ab68af866108ffdb73e3662
Sumit Saha <Sumit360saha@gmail.com>
2023-09-25 13:48:12 +0530

    first commit

---
+++ webapp/index.js
@@ -0,0 +1,16 @@
+sap.ui.define([
+	"sap/ui/core/mvc/XMLView"
+], function (XMLView) {
+	"use strict";
+
+	XMLView.create({
+		viewName: "sap.ui.demo.walkthrough.view.App",
+	}).then(function (oView) {
+		oView.placeAt("content");
+	});
+	XMLView.create({
+		viewName: "sap.ui.demo.walkthrough.fragments.AddCustomer",
+	}).then(function (oView) {
+		oView.placeAt("content");
+	});
+});
